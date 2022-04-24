b6121578c2a03801c98973ad1ff72996aa995c32
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2022-04-24 10:49:06 +0900

    Update Code.py

--- 1.DataPivot/Code.py
+++ 1.DataPivot/Code.py
@@ -3,7 +3,7 @@ import pandas as pd
 mydata = pd.read_csv("pandas_test.csv")
 
 ## prod_code별로 pivot
-
-pivoted = pd.pivot_table(mydata,index=['fact_num','WW'],columns='prod_code' ,values=['Resistance','Watt','Weight'])
+#pivoted = pd.pivot_table(mydata,index='fact_num', columns='WW' ,values=['Resistance','Watt','Weight'], aggfunc='mean')
+pivoted = mydata.pivot_table(index='Factory', columns='WW' ,values=mydata.columns[2:], aggfunc=np.min)
 rst_idx = pivoted.reset_index()
 pd.DataFrame.to_csv(pivoted,'pandas실습2.csv')
